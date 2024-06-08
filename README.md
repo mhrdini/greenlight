d764417727cbb35478676413835e3e55a6e9e608
Dini Putri <mahardini.rizky@gmail.com>
2024-06-08 16:46:31 +1000

    v1/healthcheck handler, server setup with simple loggers and config

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Within the project directory:
 
 ```bash
 > cd /cmd/api
-> go run .
-# OR
-> go run ./cmd/api
+> air # To close the server, press Ctrl+C
+```
+
+If the port is being used:
+
+```bash
+> kill $(lsof -t -i:4000) # replace 4000 with any other port number you want
 ```
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello world!")
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Port number (4000)")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	srv := &http.Server{
+		Addr:     fmt.Sprintf(":%d", cfg.port),
+		Handler:  app.routes(),
+		ErrorLog: errorLog,
+	}
+
+	infoLog.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
