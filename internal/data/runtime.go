6f8eefb55a9d37be3cc4d9d43b289d488541b586
Dini Putri <mahardini.rizky@gmail.com>
2024-06-09 14:50:48 +1000

    createMovieHandler accepts Movie with correct Runtime format, by implementing Unmarshaler interface for Runtime

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,30 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// we expect the JSON value of a Runtime data type to be in the form "<runtime> mins"
+// so we will unquote, split, and parse the int32 value of the <runtime>
+// and modify *r (the receiver) to carry that int32 value
+// NOTE: must use pointer receiver as we will modify the receiver
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
