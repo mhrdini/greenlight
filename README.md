1f679aba87f85e8c1d059f098cf7cfe877ac2085
Dini Putri <mahardini.rizky@gmail.com>
2024-06-12 01:45:26 +1000

    Setup and open PostgreSQL DB connection pool

--- README.md
+++ README.md
@@ -14,3 +14,38 @@ If the port is being used:
 ```bash
 > kill $(lsof -t -i:4000) # replace 4000 with any other port number you want
 ```
+
+## Environment variables
+
+Enter the following
+
+- `DB_USER`
+- `DB_PASSWORD`
+- `DB_NAME`
+
+### Creating the database (`DB_NAME`)
+
+```bash
+> sudo -u postgres psql
+# Within psql
+> create database <DB_NAME>;  # create the DB under the user 'postgres'
+> \c <DB_NAME>                # connect to the DB
+```
+
+### Creating the user (`DB_USER`) and password (`DB_PASSWORD`)
+
+Still within `psql`:
+
+```bash
+> create role <DB_USER> with login password '<DB_PASSWORD>';  # note the enclosing single quotes
+> create extension if not exists citext;                      # for case-insensitive fields, i.e. email
+```
+
+To ensure you've create the password-authenticated user:
+
+```bash
+> psql --host=localhost --dbname=<DB_NAME> --username=<DB_USER>
+Password:
+```
+
+And you should be within the shell after entering the password.
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -13,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -27,10 +35,22 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Port number (4000)")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	dbUser, dbPassword, dbName := os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", fmt.Sprintf("postgres://%v:%v@localhost/%v?sslmode=disable", dbUser, dbPassword, dbName), "PostgreSQL DSN")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+	infoLog.Printf("database connection pool established")
 
 	app := &application{
 		config:   cfg,
@@ -45,6 +65,28 @@ func main() {
 	}
 
 	infoLog.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database, passing in the
+	// 5-second timeout context we created above as a parameter. If the connection couldn't be
+	// established successfully within the 5 second deadline, then this will return an
+	// error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool.
+	return db, nil
 }
